e4b183a61fa16ecbfdcdc716cd44ac42f0376220
ktmud <jyyjcc@gmail.com>
2012-06-07 15:38:12 +0800

    exception msg

---
+++ conf/exception_msgs.js
@@ -0,0 +1,107 @@
+module.exports = {
+  'bad email': {
+    msg: '请输入正确的邮箱'
+  }
+  , 'no user': {
+    msg: '用户不存在',
+    type: 'warn'
+  }
+  , 'uid exists': {
+    msg: '用户已存在'
+  }
+  , 'email exists': {
+    msg: '该邮箱已注册'
+  }
+  , 'bad slug': {
+    msg: '网址只能是字母、数字和下划线的组合'
+  }
+  , 'illegal slug': {
+    msg: '网址似乎有点不和谐'
+  }
+  , 'illegal uname': {
+    msg: '名号好像有点不和谐'
+  }
+  , 'wrong password': {
+    type: 'warn',
+    msg: '密码不正确'
+  }
+  , 'password required': {
+    msg: '请输入密码'
+  }
+  , 'uname required': {
+    msg: '好歹还是让大家知道怎么称呼你吧'
+  }
+  , 'email required': {
+    msg: '请输邮箱地址'
+  }
+  , 'uid too short': {
+    msg: '用户名太短了点吧'
+  }
+  , 'uid required': {
+    msg: '请输入用户名'
+  }
+  , 'uname too short': {
+    msg: '名号至少需要两个字'
+  }
+  , 'uname too long': {
+    msg: '这么长的名字谁记得过来?'
+  }
+  , 'callback fail': {
+    msg: '任务失败，请稍后重试'
+  }
+  , 'need all field': {
+    msg: '请填写完整所有信息'
+  }
+  , 'password differ': {
+    msg: '两次输入的密码不一致'
+  }
+  , 'password2 required': {
+    msg: '需要重复密码以确认'
+  }
+  , 'weak password': {
+    msg: '密码也太简单了吧'
+  }
+  , 'save fail': {
+    msg: '保存失败，请重试'
+  }
+  , 'article conflict': {
+    msg: '发生冲突，可能是有其他人编辑过本文'
+  }
+  , 'title required': {
+    msg: '标题不能为空'
+  }
+  , 'flag detail required': {
+    msg: '请指出错误内容'
+  }
+  , 'flag reported': {
+    msg: '您已经报告过此问题了'
+  }
+  , 'flag detail too short': {
+    msg: '错误描述也太简单了吧'
+  }
+  , 'content required': {
+    msg: '内容不能为空'
+  }
+  , 'tag id required': {
+    msg: '请输入标签简写'
+  }
+  , 'tag name required': {
+    msg: '请输入标签名'
+  }
+  , 'general fail': {
+    msg: '操作失败，请重试'
+  }
+  , 'unfinished_route': {
+    msg: '请求资源未定义'
+  }
+  , 'unknown_error': {
+    msg: '发生了未知错误'
+  }
+  , 'bad request': {
+    msg: '错误的请求方式，请<a href="">刷新</a>后重试'
+  }
+  //, '': {
+    //'type': '',
+    //msg: ''
+  //}
+};
